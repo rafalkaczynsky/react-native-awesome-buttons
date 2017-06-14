a1aecfe0952ddf1245ccd9398b5110b516771442
Rafal Kaczynsky <rafalkaczynsky@Rafals-MacBook-Pro.local>
2017-06-14 15:35:33 +0100

    Added rounded buttons

--- src/windows/all-buttons.js
+++ src/windows/all-buttons.js
@@ -14,11 +14,64 @@ export default class AllButtons extends React.Component {
         const users = this.props.users // to be replaced
 
         return(
-            <View style={{flex: 1, padding: 20,}}>
+            <View style={{flex: 1, padding: 20, backgroundColor: '#e1e9f7'}}>
                 <ScrollView>
                     <Button
                         type="dialogDefault"
                         text="dialog Default"
+                        style={[StyleSheet.singleMargin]}
+                        onPress={()=>alert('Button Pressed')}
+                    />
+                    <Button
+                        type="tab"
+                        text="tab"
+                        style={[StyleSheet.singleMargin]}
+                        onPress={()=>alert('Button Pressed')}
+                    />
+                    <Button
+                        type="step"
+                        text="step"
+                        style={[StyleSheet.singleMargin]}
+                        onPress={()=>alert('Button Pressed')}
+                    />
+
+                    <Button
+                        type="rounded"
+                        text="rounded"
+                        style={[StyleSheet.singleMargin]}
+                        onPress={()=>alert('Button Pressed')}
+                    />
+
+                    <Button
+                        type="roundedGrey"
+                        text="roundedGrey"
+                        style={[StyleSheet.singleMargin]}
+                        onPress={()=>alert('Button Pressed')}
+                    />
+
+                    <Button
+                        type="roundedDefault"
+                        text="roundedDefault"
+                        style={[StyleSheet.singleMargin]}
+                        onPress={()=>alert('Button Pressed')}
+                    />
+                    <Button
+                        type="roundedWhite"
+                        text="roundedWhite"
+                        style={[StyleSheet.singleMargin]}
+                        onPress={()=>alert('Button Pressed')}
+                    />
+
+                    <Button
+                        type="roundedBorder"
+                        text="roundedBorder"
+                        style={[StyleSheet.singleMargin]}
+                        onPress={()=>alert('Button Pressed')}
+                    />
+
+                    <Button
+                        type="roundedWhiteBorder"
+                        text="roundedWhiteBorder"
                         style={[StyleSheet.singleMargin]}
                         onPress={()=>alert('Button Pressed')}
                     />
